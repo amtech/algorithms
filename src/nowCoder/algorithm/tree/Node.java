35f8be9538e03253d248e5504d811699ffa4df7b
openforever <youlovemezky@gmail.com>
2016-03-03 20:29:17 +0800

    二叉树的结点

---
+++ src/nowCoder/algorithm/tree/Node.java
@@ -0,0 +1,16 @@
+package nowCoder.algorithm.tree;
+
+/**
+ * 二叉树的结点
+ * @author snow
+ *
+ */
+public class Node {
+	public int value;
+	public Node left;
+	public Node right;
+	
+	public Node(int data) {
+		value = data;
+	}
+}
